0072cd3c6ac8114eed4b5de334e86b3b63b584fa
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-14 01:02:50 +0600

    Same Same But Different

--- result.js
+++ result.js
@@ -39,3 +39,27 @@ else {
   return "Tie";
 }
 }
+
+
+
+
+function  isSame(arr1 , arr2 ) {
+          // You have to write your code here
+
+
+  if (Array.isArray(arr1) === false) {
+  return "Invalid";
+ }
+  if (Array.isArray(arr2) === false) {
+  return "Invalid";
+ }
+      if(arr1.length !== arr2.length){
+        return false
+      }
+      for(let i=0;i<arr1.length;i++){
+       if(arr1[i]!==arr2[i]) {
+        return false
+       }
+      }
+    return true
+}
